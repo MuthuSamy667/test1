20487d874c1f882708999124f49c9ba2b48de7d0
TridentProjects <tridenttechnominds@gmail.com>
2020-08-11 11:03:20 +0530

    test add

--- Arraysofarrays.pl
+++ Arraysofarrays.pl
@@ -5,11 +5,8 @@ use warnings;
 # Initializing and defining the array 
 my @items = ( ['book', 'pen', 'pencil'], 
 			['Bat', 'Ball', 'Stumps'],
-<<<<<<< HEAD
-			['Apple', 'Orange', 'Banana'], 
-=======
 			['Apple', 'Orange', 'Banana'],
->>>>>>> 408d2db3aab63b0d5bda9406285bf5b81fa22d23
+			['Lion', 'Tiger', 'Elephant'], 
 			['snake', 'rat', 'rabbit'] ); 
 
 # Printing items from the array 
